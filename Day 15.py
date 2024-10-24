69bbd33504f0b3717201a7b205e0e6545b373377
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2024-10-24 18:04:02 +0530

    Update and rename Day 15.txt to Day 15.py

--- Day 15.py
+++ Day 15.py
@@ -1,5 +1,21 @@
+"""
 Day 15 coding Statement:
 Write a program to identify if the number is Strong number or not
+
+Get a number as input from user and then check whether that number is a strong number or not. A number is said to be strong number if the sum of the factorial of each digit in the number is same as that of the number.
+
+E.g. let the number be 145
+
+Here 1! + 4! + 5! is 1 + 24 + 120 which is equal to 145 itself.
+
+Input
+
+121
+
+Output
+
+Not a strong number
+"""
 
 
 # Function to calculate factorial of a number

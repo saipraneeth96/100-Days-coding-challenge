b1ae4b813e5911455f8bfb9ba2dcd1c55a0e2d91
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2024-10-26 20:24:39 +0530

    Update and rename Day 16.txt to Day 16.py

--- Day 16.py
+++ Day 16.py
@@ -1,6 +1,13 @@
+"""
 Day 16 coding Statement:
 Write a program to identify if the number is Perfect number or not
 
+Description
+
+Get the input from the user and check whether that number is a perfect number or not.
+
+E.g. Let number is 28, factors of 28 are 1,2,4,7,14. Now the sum of all these factors are 28 itself.
+"""
 
 # input a number
 num = int(input("Enter a number: "))

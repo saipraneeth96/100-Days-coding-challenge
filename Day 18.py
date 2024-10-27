337f028fba20686f9995f2acbac065f49673fee5
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2024-10-27 17:49:29 +0530

    Update and rename Day 18.txt to Day 18.py

--- Day 18.py
+++ Day 18.py
@@ -1,5 +1,15 @@
+"""
 Day 18 coding Statement:
 Write a program to Add two fractions
+
+Description
+
+Get the values for numerator and denominator of two fractions, then add that fractions. Consider the following format
+
+x3/y3 = (x1/y1) + (x2/y2)
+
+here x3 = (x1*y2) + (x2*y1) and y3 = (y1*y2)
+"""
 
 
 import math

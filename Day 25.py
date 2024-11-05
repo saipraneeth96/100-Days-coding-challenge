8b54214ad929db9955dfe47abc16476202c22b91
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2024-11-05 19:50:14 +0530

    Update and rename Day 25.txt to Day 25.py

--- Day 25.py
+++ Day 25.py
@@ -1,6 +1,16 @@
+"""
 Day 25 coding Statement:
 Write a program to find Area of a circle
 
+Description:
+Get the value for radius from the user and calculate the area of the circle for the given radius.
+
+Area of circle = 3.14*radius*radius
+
+Input: 3
+
+Output: 28.26
+"""
 
 # input the radius of circle
 radius = float(input("Enter the radius of number: "))

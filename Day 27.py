96460d1b2ddeeafba1267d6730a6afcba187dd9a
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2024-11-08 08:48:59 +0530

    Update and rename Day 27.txt to Day 27.py

--- Day 27.py
+++ Day 27.py
@@ -1,5 +1,7 @@
+"""
 Day 27 coding Statement:
 Write a program to find the double of the given number without using arithmetic operator
+"""
 
 # input a number
 num = int(input("Enter a number: "))

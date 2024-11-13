a79df709e7ac2921c39e6231e2f3a29f493d8803
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2024-11-13 15:59:51 +0530

    Update and rename Day 31.txt to Day 31.py

--- Day 31.py
+++ Day 31.py
@@ -1,5 +1,10 @@
+"""
 Day 31 coding Statement:
 Write a Program to Toggle each character in a string
+
+Description
+Get an input string from user and then convert the lower case of alphabets to upper case and all upper-case alphabets into lower case.
+"""
 
 # Original string
 text = input("Enter a string: ")

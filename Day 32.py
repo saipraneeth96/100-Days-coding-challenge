258eeb8274c44a22f449d595c7dc0d6f9fd835a1
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2024-11-14 08:00:46 +0530

    Update and rename Day 32.txt to Day 32.py

--- Day 32.py
+++ Day 32.py
@@ -1,5 +1,7 @@
+"""
 Day 32 coding Statement:
 Write a Program to Remove vowels from a string
+"""
 
 # input a string
 text = input("Enter a string: ")
@@ -8,9 +10,10 @@ vowels = "aeiouAEIOU"
 
 # loop through each character
 for char in text:
-    # if current chaRACTER IS A VOWEL
+    # if current character is a vowel
     if char in vowels:
         # remove this character
         text = text.replace(char, "")
-        
+
+# display the result
 print(text) 

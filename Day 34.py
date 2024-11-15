bf78b83602c7d67f97c8873aeeacbc27dcef2242
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2024-11-15 12:05:31 +0530

    Update and rename Day 34.txt to Day 34.py

--- Day 34.py
+++ Day 34.py
@@ -1,5 +1,13 @@
+"""
 Day 34 coding Statement:
 Write a Program to Remove brackets from an algebraic expression
+
+Description:
+Get an algebraic expression as input from the user and then remove all the brackets in that.
+
+Input: 7x+(2*y)
+Output: 7x+2*y
+"""
 
 
 # input an algebric expression as string

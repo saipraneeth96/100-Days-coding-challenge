74cb5d04b6c9d15c26f9372fa205fce85a00acde
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2024-11-18 18:18:58 +0530

    Update and rename Day 37.txt to Day 37.py

--- Day 37.py
+++ Day 37.py
@@ -1,5 +1,19 @@
+"""
 Day 37 coding Statement:
 Write a Program to calculate the Frequency of characters in a string
+
+Description: Get a string as the input from the user and find the frequency of characters in the string.
+
+Input: program
+
+Output:
+The frequency of a is 1
+The frequency of g is 1
+The frequency of m is 1
+The frequency of o is 1
+The frequency of p is 1
+The frequency of r is 2
+"""
 
 # input a string
 s = input("Enter s atring: ")

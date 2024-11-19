848b3396ac66f0d72da5d4430fd08c5495171977
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2024-11-19 12:00:26 +0530

    Update and rename Day 38.txt to Day 38.py

--- Day 38.py
+++ Day 38.py
@@ -1,5 +1,14 @@
+"""
 Day 38 coding Statement:
 Write a Program to print Non-repeating characters in a string
+
+Description: Get a string as the input from the user and print the non-repeating characters in a string.
+
+Input: Hello
+
+Output: H e o
+
+"""
 
 
 # input a string

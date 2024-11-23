5710375214e7d0a82d97885bcc593d67a7f08346
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2024-11-23 11:13:37 +0530

    Update and rename Day 41.txt to Day 41.py

--- Day 41.py
+++ Day 41.py
@@ -1,5 +1,20 @@
+"""
 Day 41 coding Statement:
 Check if two strings match where one string contains wildcard characters
+
+Description
+Get two strings as input from the user, first with wildcard characters (* and ?) and second without wildcard characters.
+Then check whether they match or not.
+
+Input:
+
+Ta**nt
+Talent
+
+Output: Yes they match
+
+"""
+
 
 
 # Input a string with wildcard characters

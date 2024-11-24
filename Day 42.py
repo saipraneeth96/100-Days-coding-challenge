ec9769bec16952787e753a6f249ba4d971df6d7a
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2024-11-24 18:07:24 +0530

    Update and rename Day 42.txt to Day 42.py

--- Day 42.py
+++ Day 42.py
@@ -1,5 +1,34 @@
+"""
 Day 42 coding Statement:
 Write Program to check if two arrays are the same or not
+
+Description
+
+Get two arrays as the input from the user and check whether it is the same or not.
+
+Input
+
+Enter the size of first array :
+
+3
+
+Enter the size of second array :
+
+3
+
+Enter elements of first array :
+
+1 2 3
+
+Enter elements of second array :
+
+1 2 3
+
+Output
+
+Same
+"""
+
 
 l1 = int(input("Enter the size of first array: "))
 l2 = int(input("Enter the size of second array: "))

b868e1e3cbee9be3929404b9071681c5bcbde39e
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2024-11-26 18:41:04 +0530

    Update and rename Day 43.txt to Day 43.py

--- Day 43.py
+++ Day 43.py
@@ -1,5 +1,18 @@
-Day 43 coding Statement:
+"""
+\Day 43 coding Statement:
 Write Program to find the array type
+
+Description: Get an array as input from the user and check the type of the array, whether it is odd, even or mixed type.
+
+Input
+
+Enter size of array : 3
+
+Enter elements 
+1 3 5
+
+Output: Odd
+"""
 
 # input size of the array
 n = int(input("Enter size of the array: "))

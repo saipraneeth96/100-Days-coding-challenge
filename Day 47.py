b37064b730c2185d641f1f5c0fe1a894620359e6
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2024-11-29 11:53:35 +0530

    Update Day 47.py

--- Day 47.py
+++ Day 47.py
@@ -1,5 +1,18 @@
+"""
 Day 47 coding Statement:
 Write Program to find longest palindrome in an array
+Description: Get an array as the input from the user and find the longest palindrome in that array.
+
+Input
+
+Enter the size of array: 3
+
+Enter the elements of array
+
+121 10456 1000001
+
+Output: 1000001
+"""
 
 
 def is_palindrome(number):

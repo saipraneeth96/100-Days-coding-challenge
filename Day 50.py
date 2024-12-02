8aa475bc5e15235351774de1de1458a64419691b
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2024-12-02 13:34:32 +0530

    Update Day 50.py

--- Day 50.py
+++ Day 50.py
@@ -1,5 +1,31 @@
+"""
 Day 50 coding Statement:
 Given an integer array of size N. Write Program to find sum of positive square elements in the array.
+
+Sample input 1 :
+
+4 1 2 3 4
+
+Sample output 1 :
+
+30
+
+Explanation :
+
+(1 + 4 + 9 + 16) = 30
+
+Sample input 2 :
+
+4 -1 -2 -3 -4
+
+Sample output 2 :
+
+30
+
+Explanation :
+
+(1 + 4 + 9 + 16) = 30
+"""
 
 size = int(input("Enter size of the array: "))
 

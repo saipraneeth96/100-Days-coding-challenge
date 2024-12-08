5e19f1beb4b885d391eda9f770704d952ac3a298
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2024-12-08 13:02:17 +0530

    Update Day 56.py

--- Day 56.py
+++ Day 56.py
@@ -1,5 +1,24 @@
+"""
 Day 56 coding Statement:
 Write Program to find whether the numbers of an array be made equal
+Description
+
+Check whether the numbers of array be made equal or not
+
+For eg, for the following input it should print yes because
+
+50*2*3 , 75*2*2 and 150*2 are equal to 300 in all cases. So array numbers can be made equal
+
+Input
+
+3
+
+50 75 150
+
+Output
+
+Yes
+"""
 
 def can_make_equal(arr):
     # Step 1: Normalize each number by dividing out factors of 2 and 3

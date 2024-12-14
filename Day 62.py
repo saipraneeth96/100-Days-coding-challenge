c217595e0fb4347051741dbd0c5b5d02f34e532a
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2024-12-14 12:57:22 +0530

    Update Day 62.py

--- Day 62.py
+++ Day 62.py
@@ -1,3 +1,4 @@
+"""
 Day 62 coding Statement : Gold Mining
 
 Anusree has decided to go to a gold mine along with N of his friends (thus, total N+1 people including Anusree go to the gold mine).
@@ -6,7 +7,36 @@ The gold mine contains a total of X kg of gold. Every person has the capacity of
 
 Will Anusree and his friends together be able to carry up all the gold from the gold mine assuming that they can go to the mine exactly once.
 
+Input Format
+
+First line will contain T, number of testcases. Then the testcases follow. Each testcase contains of a single line of input, three integers N, X, Y.
+
+Output Format
+
+For each testcase, output "YES" if you and your friends can carry all the gold, otherwise output "NO".
+
+You may print each character of the string in uppercase or lowercase (for example, the strings "yEs", "yes", "Yes" and "YES" will all be treated as identical).
+
+Sample Input 1
+
+3
+
+2 10 3
+
+2 10 4
+
+1 5 10
+
+Sample Output 1
+
+NO
+
+YES
+
+YES
+
 SOLUTION:
+"""
 
 # enter number of testcases
 t = int(input())

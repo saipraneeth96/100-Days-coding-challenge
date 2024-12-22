4c4e3beb7bb158b213084f25f4682d53e2623465
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2024-12-22 14:26:28 +0530

    Update and rename Day 70.txt to Day 70.py

--- Day 70.py
+++ Day 70.py
@@ -1,5 +1,31 @@
+"""
 Day 70 coding Statement:
 Given an array, rotate the array by one position in clock-wise direction.
+
+Example 1:
+
+Input:
+
+N = 5
+
+A[] = {1, 2, 3, 4, 5}
+
+Output:
+
+5 1 2 3 4
+
+Example 2:
+
+Input:
+
+N = 8
+
+A[] = {9, 8, 7, 6, 4, 2, 1, 3}
+
+Output:
+
+3 9 8 7 6 4 2 1
+"""
 
 # input size of the array
 n = int(input())

8939c457348a2873a028eab9a1d8ed6c2f71668f
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2024-12-24 19:53:56 +0530

    Update Day 72.py

--- Day 72.py
+++ Day 72.py
@@ -1,5 +1,40 @@
+"""
 Day 72 coding Statement:
-In this problem, you will have to implement a simple editor. The editor maintains the content of a string S and have two following functions:
+In this problem, you will have to implement a simple editor. The editor maintains the content of a string S and have two following functions
+
+"+ i x": insert a string x into the current string S after the i'th character of the S (we use 1-indexing in this problem).
+When i equals to 0 it mean we add x at the beginning of S.
+"? i len": Print the sub-string of length len starting at position i'th of S.
+At the beginning, the editor holds an empty string. There will be Q queries of the two types described above.
+
+Input
+
+The first line contains the integer Q. Each line in the next Q lines contains one query.
+
+Output
+
+For each query of the second type, print out the answer sub-string in one line.
+
+Sample Input
+
+5
+
++ 0 ab
+
++ 1 c
+
+? 1 3
+
++ 2 dd
+
+? 1 5
+
+Sample Output
+
+acb
+
+acddb
+"""
 
 # Function to process queries
 def process_queries(queries):

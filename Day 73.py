534a58cf11abab6d4d7a7e7f99c3323b23b9d2b4
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2024-12-25 09:36:27 +0530

    Update and rename Day 73.txt to Day 73.py

--- Day 73.py
+++ Day 73.py
@@ -1,4 +1,61 @@
+"""
 Day 73 coding Statement: Boring String
+
+Day 73 coding Statement : 
+
+A string is called boring if all the characters of the string are same.
+
+You are given a string S of length N, consisting of lowercase english alphabets. 
+Find the length of the longest boring substring of S which occurs more than once.
+
+Note that if there is no boring substring which occurs more than once in S, the answer will be 00.
+
+A substring is obtained by deleting some (possibly zero) elements from the beginning of the string and some (possibly zero) elements from the end of the string.
+
+Input Format
+
+The first line of input will contain a single integer T, denoting the number of test cases.
+Each test case consists of two lines of input.
+The first line of each test case contains an integer N, denoting the length of string S.
+The next contains string S.
+Output Format
+
+For each test case, output on a new line, the length of the longest boring substring of S which occurs more than once.
+
+ 
+
+Sample Input
+
+4
+
+3
+
+aaa
+
+3
+
+abc
+
+5
+
+bcaca
+
+6
+
+caabaa
+
+ 
+
+Sample Output
+
+2
+
+0
+
+1
+
+2
+"""
 
 def find_longest_boring_substring(T, test_cases):
     results = []

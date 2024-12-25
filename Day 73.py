d9ea320bb04d3ce5e7eb39dd03292d82487d15e9
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2024-12-25 09:37:26 +0530

    Update Day 73.py

--- Day 73.py
+++ Day 73.py
@@ -10,7 +10,8 @@ Find the length of the longest boring substring of S which occurs more than once
 
 Note that if there is no boring substring which occurs more than once in S, the answer will be 00.
 
-A substring is obtained by deleting some (possibly zero) elements from the beginning of the string and some (possibly zero) elements from the end of the string.
+A substring is obtained by deleting some (possibly zero) elements from the beginning of the string and some (possibly zero)
+elements from the end of the string.
 
 Input Format
 
@@ -25,35 +26,22 @@ For each test case, output on a new line, the length of the longest boring subst
  
 
 Sample Input
-
 4
-
 3
-
 aaa
-
 3
-
 abc
-
 5
-
 bcaca
-
 6
-
 caabaa
 
  
 
 Sample Output
-
 2
-
 0
-
 1
-
 2
 """
 

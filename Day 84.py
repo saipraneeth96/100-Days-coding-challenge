c548d5de510cf098d6b53c7fa81b04b4ffc8b67f
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2025-01-06 11:51:00 +0530

    Update Day 84.py

--- Day 84.py
+++ Day 84.py
@@ -1,6 +1,33 @@
+"""
 Day 84 coding Statement : 
 
-Given an undirected graph and an integer M. The task is to determine if the graph can be colored with at most M colors such that no two adjacent vertices of the graph are colored with the same color. Here coloring of a graph means the assignment of colors to all vertices. Print 1 if it is possible to colour vertices and 0 otherwise.
+Given an undirected graph and an integer M. 
+The task is to determine if the graph can be colored with at most M colors
+such that no two adjacent vertices of the graph are colored with the same color.
+Here coloring of a graph means the assignment of colors to all vertices. 
+Print 1 if it is possible to colour vertices and 0 otherwise.
+
+Example 1:
+
+Input:
+N = 4
+M = 3
+E = 5
+Edges[] = {(0,1),(1,2),(2,3),(3,0),(0,2)}
+
+Output: 1
+Explanation: It is possible to colour the given graph using 3 colours.
+
+Example 2:
+
+Input:
+N = 3
+M = 2
+E = 3
+Edges[] = {(0,1),(1,2),(0,2)}
+Output: 0
+
+"""
 
 def is_valid_color(node, color, graph, colors):
     for neighbor in graph[node]:

af550c4364e38b6187f9c3e47d115e74666614da
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2025-01-09 17:16:28 +0530

    Update Day 86.py

--- Day 86.py
+++ Day 86.py
@@ -1,6 +1,42 @@
+"""
 Day 86 coding Statement : 
 
-Mahesh got a beautiful array named A as a birthday gift from his beautiful girlfriend Namratha. There are N positive integers in that array. Mahesh loved the array so much that he started to spend a lot of time on it everyday. One day, he wrote down all possible subsets of the array. Then for each subset, he calculated the sum of elements in that subset and wrote it down on a paper. Unfortunately, Mahesh lost the beautiful array :(. He still has the paper on which he wrote all subset sums. Your task is to rebuild beautiful array A and help the couple stay happy :)
+Mahesh got a beautiful array named A as a birthday gift from his beautiful girlfriend Namratha. 
+There are N positive integers in that array. Mahesh loved the array so much that he started to spend a lot of time on it everyday.
+One day, he wrote down all possible subsets of the array. 
+Then for each subset, he calculated the sum of elements in that subset and wrote it down on a paper. 
+Unfortunately, Mahesh lost the beautiful array :(. He still has the paper on which he wrote all subset sums. 
+Your task is to rebuild beautiful array A and help the couple stay happy :)
+
+Input
+
+The first line of the input contains an integer T denoting the number of test cases. 
+First line of each test case contains one integer N, the number of elements in A. 
+Second line of each test case contains 2^N integers, the values written on paper
+
+Output
+
+For each test case, output one line with N space separated integers in non-decreasing order.
+
+Sample Input
+
+2
+
+1
+
+0 10
+
+2
+
+0 1 1 2
+
+Sample Output
+
+10
+
+1 1
+
+"""
 
 def reconstruct_array(T, test_cases):
     results = []

96afc086059635810de83a16b72d81ff1485e88d
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2025-01-17 18:40:00 +0530

    Update Day 94.py

--- Day 94.py
+++ Day 94.py
@@ -1,27 +1,33 @@
 """
 Day 94 coding Statement : 
 
-Kekocity's population consist of N gnomes numbered with unique ids from 1 to N. As they are very joyful gnomes, they usually send jokes to their friends right after they get any (even if they knew it before) via their social network named as Mybeard. Mybeard became popular in the city because of message auto-deletion. It takes exactly one minute to read and resend joke to mates.
-
-Mayor of Kekocity, Mr. Shaikhinidin, is interested in understanding how the jokes are spread. He gives you database of Mybeard social network, and wants you to answer some queries on it.
-
-You will be given a list of friends for every gnome and M queries of the following type: Who will receive a message with joke after exactly K minutes, if the creator of joke was gnome with id x?
+Kekocity's population consist of N gnomes numbered with unique ids from 1 to N. As they are very joyful gnomes, 
+they usually send jokes to their friends right after they get any (even if they knew it before) via their social network
+named as Mybeard. Mybeard became popular in the city because of message auto-deletion.
+It takes exactly one minute to read and resend joke to mates.
+\Mayor of Kekocity, Mr. Shaikhinidin, is interested in understanding how the jokes are spread. He gives you database of Mybeard 
+social network, and wants you to answer some queries on it.
+You will be given a list of friends for every gnome and M queries of the following type: Who will receive a message 
+with joke after exactly K minutes, if the creator of joke was gnome with id x?
 
 Input
 
 The first line contains a single integer N denoting the number of gnomes.
 
-The next N lines contain the the matrix g[N][N]. Each of the i-th line, will contain N space separated integers - j-th of those will denote g[i][j]. If gnome j is friend of gnome i, then g[i][j] is 1. Otherwise it will be zero. Plese note that the friendship relationship is not bidirectional, i.e. it might happen that g[i][j] may not be equal to g[j][i]. Also one can be friend of itself also, i.e. g[i][i] may be equal to 1.
-
-The next line contains a single integer M denoting the number of queries. The next M lines contain two integers k and x described above.
+The next N lines contain the the matrix g[N][N]. Each of the i-th line,
+will contain N space separated integers - j-th of those will denote g[i][j]. If gnome j is friend of gnome i, 
+then g[i][j] is 1. Otherwise it will be zero. Plese note that the friendship relationship is not bidirectional,
+i.e. it might happen that g[i][j] may not be equal to g[j][i]. Also one can be friend of itself also, 
+i.e. g[i][i] may be equal to 1.
+The next line contains a single integer M denoting the number of queries.
+The next M lines contain two integers k and x described above.
 
 Output
 
 For each query, output two lines.
-
 In the first line, output how many gnomes will know the joke after k minutes.
-
-In the second line, print these ids (numbering) of these gnomes in increasing order. If no one will know the joke after K minutes, then print -1 in this line.
+In the second line, print these ids (numbering) of these gnomes in increasing order. 
+If no one will know the joke after K minutes, then print -1 in this line.
 
 Constraints
 

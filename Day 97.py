8ea5f29ba2b4958aad465900afedaa7e9a3362e5
Deepak Sai Praneeth Pegaredla <116938445+saipraneeth96@users.noreply.github.com>
2025-01-20 18:59:48 +0530

    Updated Day 97.py

--- Day 97.py
+++ Day 97.py
@@ -1,3 +1,4 @@
+"""
 Day 97 coding Statement : 
 
 Arun has a rooted tree of N vertices rooted at vertex 1. Each vertex can either be coloured black or white.
@@ -8,7 +9,62 @@ Arun can perform the following operation any number of times. In one operation, 
 
 Help Arun find the minimum number of operations required to change the colouring of the vertices to B1?,B2?,…BN? respectively.
 
+Input Format
 
+The first line contains a single integer T — the number of test cases. Then the test cases follow.
+The first line of each test case contains an integer N — the size of the tree.
+The second line of each test case contains N space-separated integers A1?,A2?,…,AN? denoting the initial colouring of the vertices of the tree.
+The third line of each test case contains N space-separated integers B1?,B2?,…,BN? denoting the final desired colouring of the vertices of the tree.
+The next N−1 lines contain two space-separated integers u and v — denoting an undirected edge between nodes u and v.
+It is guaranteed that the edges given in the input form a tree.
+
+Output Format
+
+For each testcase, output the minimum number of operations required to obtain the desired colouring.
+
+ 
+
+Sample Input
+
+2
+
+4
+
+1 1 0 0
+
+1 1 1 0
+
+1 2
+
+1 3
+
+1 4
+
+5
+
+1 1 1 0 0
+
+1 0 1 1 1
+
+5 3
+
+3 1
+
+2 1
+
+4 3
+
+ 
+
+Sample Output
+
+1
+
+2
+
+ 
+
+"""
 
 from collections import defaultdict
 import sys
